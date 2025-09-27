636b41726cd0d828fcff1c9cd319d8a7f7ba3160
Teia <teia_canini@icloud.com>
2025-09-27 17:41:37 -0400

    set up react

---
+++ scdt-website/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Stevens Cyber Defense Team
+      </header>
+    </div>
+  );
+}
+
+export default App;
